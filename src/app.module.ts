8cc931c8bf59573f259936a2163d5d3038a6d361
hiderbrandon <hiderbrandon@gmail.com>
2022-09-01 15:31:03 -0500

    simple programmatic conection running

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { Client } from 'pg';
+
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +9,17 @@ import { ClientsModule } from './clients/clients.module';
 import config from './config';
 import { environments } from './environments';
 
+const client = new Client({
+  user: `root`,
+  host: `localhost`,
+  database: `my_db`,
+  password: `123456`,
+  port: 5432
+})
 
+client.connect()
+
+client.query(`SELECT * FROM client`)
 
 @Module({
   imports: [
